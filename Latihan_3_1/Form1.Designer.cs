9a450594bf95ee058bb2b0c27914c83b978885fe
Calwin141111294 <calwinchen96@gmail.com>
2016-10-12 23:29:00 +0700

    Latihan Minggu 3

---
+++ Latihan_3_1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Latihan_3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolBold = new System.Windows.Forms.ToolStripButton();
+            this.toolItalic = new System.Windows.Forms.ToolStripButton();
+            this.toolUnderline = new System.Windows.Forms.ToolStripButton();
+            this.cmbSize = new System.Windows.Forms.ToolStripComboBox();
+            this.cmbFont = new System.Windows.Forms.ToolStripComboBox();
+            this.btnColor = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-2, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(476, 316);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolBold,
+            this.toolItalic,
+            this.toolUnderline,
+            this.cmbSize,
+            this.cmbFont,
+            this.btnColor});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(474, 25);
+            this.toolStrip1.TabIndex = 6;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolBold
+            // 
+            this.toolBold.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolBold.Image = ((System.Drawing.Image)(resources.GetObject("toolBold.Image")));
+            this.toolBold.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolBold.Name = "toolBold";
+            this.toolBold.Size = new System.Drawing.Size(23, 22);
+            this.toolBold.Text = "toolStripButton1";
+            this.toolBold.Click += new System.EventHandler(this.toolBold_Click_1);
+            // 
+            // toolItalic
+            // 
+            this.toolItalic.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolItalic.Image = ((System.Drawing.Image)(resources.GetObject("toolItalic.Image")));
+            this.toolItalic.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolItalic.Name = "toolItalic";
+            this.toolItalic.Size = new System.Drawing.Size(23, 22);
+            this.toolItalic.Text = "toolStripButton2";
+            this.toolItalic.Click += new System.EventHandler(this.toolItalic_Click);
+            // 
+            // toolUnderline
+            // 
+            this.toolUnderline.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolUnderline.Image = ((System.Drawing.Image)(resources.GetObject("toolUnderline.Image")));
+            this.toolUnderline.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolUnderline.Name = "toolUnderline";
+            this.toolUnderline.Size = new System.Drawing.Size(23, 22);
+            this.toolUnderline.Text = "toolStripButton3";
+            this.toolUnderline.Click += new System.EventHandler(this.toolUnderline_Click);
+            // 
+            // cmbSize
+            // 
+            this.cmbSize.Name = "cmbSize";
+            this.cmbSize.Size = new System.Drawing.Size(121, 25);
+            this.cmbSize.SelectedIndexChanged += new System.EventHandler(this.cmbSize_SelectedIndexChanged);
+            this.cmbSize.Click += new System.EventHandler(this.cmbSize_Click);
+            // 
+            // cmbFont
+            // 
+            this.cmbFont.Name = "cmbFont";
+            this.cmbFont.Size = new System.Drawing.Size(121, 25);
+            this.cmbFont.SelectedIndexChanged += new System.EventHandler(this.cmbFont_SelectedIndexChanged);
+            // 
+            // btnColor
+            // 
+            this.btnColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnColor.Image = ((System.Drawing.Image)(resources.GetObject("btnColor.Image")));
+            this.btnColor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(23, 22);
+            this.btnColor.Text = "Color";
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 344);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Latihan_3_1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolBold;
+        private System.Windows.Forms.ToolStripButton toolItalic;
+        private System.Windows.Forms.ToolStripButton toolUnderline;
+        private System.Windows.Forms.ToolStripComboBox cmbSize;
+        private System.Windows.Forms.ToolStripComboBox cmbFont;
+        private System.Windows.Forms.ToolStripButton btnColor;
+    }
+}
+
---
+++ Latihan_3_1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Latihan_3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(FontFamily font in FontFamily.Families)
+            {
+                cmbFont.Items.Add(font.Name.ToString());
+            }
+            for (int i = 8; i <= 72; i += 2)
+            {
+                cmbSize.Items.Add(i);
+            }
+            
+            cmbSize.SelectedIndex = 2;
+            cmbFont.Text = "Times New Roman";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void cmbSize_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            var color = new ColorDialog();
+            color.ShowDialog();
+            richTextBox1.ForeColor = color.Color;
+        }
+
+        private void toolUnderline_Click(object sender, EventArgs e)
+        {
+            Font new1, old;
+            old = richTextBox1.Select
+            ionFont;
+            if (old.Underline)
+            {
+                new1 = new Font(old, old.Style & ~FontStyle.Underline);
+                toolUnderline.Checked = false;
+            }
+            else
+            {
+                new1 = new Font(old, old.Style | FontStyle.Underline);
+                toolUnderline.Checked = true;
+            }
+            richTextBox1.SelectionFont = new1;
+        }
+
+        private void toolBold_Click_1(object sender, EventArgs e)
+        {
+            Font new1, old;
+            old = richTextBox1.SelectionFont;
+            if (old.Bold)
+            {
+                new1 = new Font(old, old.Style & ~FontStyle.Bold);
+                toolBold.Checked = false;
+            }
+            else
+            {
+                new1 = new Font(old, old.Style | FontStyle.Bold);
+                toolBold.Checked = true;
+            }
+            richTextBox1.SelectionFont = new1;
+        }
+
+        private void toolItalic_Click(object sender, EventArgs e)
+        {
+            Font new1, old;
+            old = richTextBox1.SelectionFont;
+            if (old.Italic)
+            {
+                new1 = new Font(old, old.Style & ~FontStyle.Italic);
+                toolItalic.Checked = false;
+            }
+            else
+            {
+                new1 = new Font(old, old.Style | FontStyle.Italic);
+                toolItalic.Checked = true;
+            }
+            richTextBox1.SelectionFont = new1;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void cmbSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          richTextBox1.Font = new Font(richTextBox1.Font.FontFamily, float.Parse(cmbSize.SelectedItem.ToString()));
+        }
+
+        private void cmbFont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(cmbFont.SelectedItem.ToString(), richTextBox1.Font.Size);
+        }
+    }
+}
