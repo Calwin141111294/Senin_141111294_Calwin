2f1b9a0519783ae287a34365bf6d9d568cbbfd07
Calwin141111294 <calwinchen96@gmail.com>
2016-12-08 23:40:12 +0700

    Penambahan Latihan POS

---
+++ Latihan_POS/formHome.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Latihan_POS
+{
+    partial class formHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.registrasiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolBarang = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registrasiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(431, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // registrasiToolStripMenuItem
+            // 
+            this.registrasiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolBarang});
+            this.registrasiToolStripMenuItem.Name = "registrasiToolStripMenuItem";
+            this.registrasiToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.registrasiToolStripMenuItem.Text = "Registrasi";
+            // 
+            // toolBarang
+            // 
+            this.toolBarang.Name = "toolBarang";
+            this.toolBarang.Size = new System.Drawing.Size(152, 22);
+            this.toolBarang.Text = "Barang";
+            this.toolBarang.Click += new System.EventHandler(this.toolBarang_Click);
+            // 
+            // formHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "formHome";
+            this.Text = "formHome";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem registrasiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolBarang;
+    }
+}
---
+++ Latihan_POS/formRegistrasi.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace Latihan_POS
+{
+    public partial class formRegistrasi : Form
+    {
+        MySqlConnection conn;
+        String connString = "Server=Localhost; Database=barang; Uid=root; pwd=''";
+        MySqlCommand cmd;
+        public formRegistrasi()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(connString);
+        }
+
+        private void formRegistrasi_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form Home = new formHome();
+            Home.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+        private bool Angka(String angka)
+        {
+
+            for (int i = 0; i < angka.Length; i++)
+                if ((int)angka[i] < 48 || (int)angka[i] > 57)
+                    return false;
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String kode = txtKode.Text,
+                   nama = txtNama.Text,
+                   jumlah = txtJumlah.Text,
+                   hargaAwal = txtModal.Text,
+                   hargaJual = txtJual.Text;
+            if(kode =="")
+            {
+                MessageBox.Show("Kode Tidak boleh kosong");
+                return;
+            }
+            if( nama =="")
+            {
+                MessageBox.Show("Nama Tidak boleh kosong");
+                return;
+            }
+            if( jumlah =="" || !Angka(jumlah))
+             {
+                 MessageBox.Show("Jumlah isi dengan angka");
+                 return;
+             }
+            if( hargaAwal=="" || !Angka(hargaAwal))
+            {
+                MessageBox.Show("Harga isi dengan angka");
+                return;
+            }
+            if( hargaJual=="" || !Angka(hargaJual))
+            {
+                MessageBox.Show("Harga isi dengan angka");
+                return;
+            }
+            try
+            {
+                conn.Open();
+                DateTime sekarang = DateTime.Now;
+                String waktuNow = sekarang.ToString("yyyy/MM/dd HH:mm:ss");
+                DateTime update = DateTime.Now;
+                String waktuUpdate = update.ToString("yyyy/MM/dd HH:mm:ss");
+                String values = "VALUES ('" + kode + "', '" + nama + "', '" + jumlah + "', '" + Convert.ToDecimal(hargaAwal) + "', '" + Convert.ToDecimal(hargaJual) + "','" + waktuNow + "','" + waktuUpdate + "' );";
+                MessageBox.Show(values);
+                String sql = "INSERT INTO tblbarang(kode,nama,jumlahawal,hargahpp,hargaJual,createdate,updatedate) " + values;
+                cmd = new MySqlCommand(sql, conn);   
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Tambah Barang Sukses");       
+            }
+            catch
+            {
+                MessageBox.Show("DB Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+    }
+}
